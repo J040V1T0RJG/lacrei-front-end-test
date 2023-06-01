dc0c9c62e655cc357d87cfbc0ae96e8b4cc2afbf
João Vitor <joaovitor.jovg73@gmail.com>
2023-06-01 16:41:04 -0300

    :sparkles: feat: setup styled-components

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { GlobalStyle } from '@/styles/global'
+import { defaultTheme } from '@/styles/themes/default'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+import { ThemeProvider } from 'styled-components'
+
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunito.className}>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
+  )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  white: '#fff',
+  black: '#000',
+
+  'gray-100': '#EEEEEE',
+  'gray-300': '#515151',
+  'gray-900': '#1F1F1F',
+
+  'green-300': '#00B37E',
+  'green-500': '#018762',
+} as const
