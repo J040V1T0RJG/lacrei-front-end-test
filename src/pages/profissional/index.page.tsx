22575fcddf251f24adb0497075f917347bccc3b2
João Vitor <joaovitor.jovg73@gmail.com>
2023-06-02 12:38:33 -0300

    :sparkles: feat: professional page

--- src/pages/profissional/index.page.tsx
+++ src/pages/profissional/index.page.tsx
@@ -1,3 +1,19 @@
+import { MainPageInfo } from '@/components/MainPageInfo'
+import { ProfessionalContainer } from './styles'
+import Image from 'next/image'
+import professionalCoverImage from '@/assets/professionalCover.svg'
+
 export default function Professional() {
-  return <p>Professional</p>
+  return (
+    <ProfessionalContainer>
+      <MainPageInfo
+        title="Profissional"
+        infoText="A Lacrei garante que pessoas LGBTQIAPN + recebam atendimento realizado por profissionais de qualidade e que atendam às suas necessidades de forma segura e acolhedora."
+      />
+      <Image
+        src={professionalCoverImage}
+        alt="Imagem ilustrativa de profissionais da área da saúde em diálogo entre si."
+      />
+    </ProfessionalContainer>
+  )
 }
